4598d198431b8cbff408cce46428d4d0e25a2396
LuPan2003 <dungpvph24821@fpt.edu.vn>
2023-09-26 16:17:22 +0700

    Man hinh LoginActivity

---
+++ app/src/main/java/com/example/app_nghe_nhac_soundbox_md31/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.app_nghe_nhac_soundbox_md31.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_nghe_nhac_soundbox_md31.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
