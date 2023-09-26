799d2a8db5b0bb3dde0cfdff5acb63a53e054aa4
LuPan2003 <dungpvph24821@fpt.edu.vn>
2023-09-26 15:03:40 +0700

    Man hinh SplashActivity

---
+++ app/src/main/java/com/example/app_nghe_nhac_soundbox_md31/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_nghe_nhac_soundbox_md31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
